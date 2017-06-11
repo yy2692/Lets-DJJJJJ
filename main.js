774e4589d03119621de2d40535c4fea461bd41b3
yy2692 <yy2692@tc.columbia.edu>
2017-06-11 00:28:43 -0400

    change June/11

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
+function goToNextVideo() {
+  var videoEl = document.querySelector('#tutorialVideo');
+  videoEl.src = "https://www.youtube.com/embed/eLDe2sGV4RU?list=PL_YqEG_6NcDsHudcr-jjgMs_tVhrK2nbw";
+  //I want to build a loop on this goToNextVideo, but I failed the task changeImage loop in Tinkering. Also no idea this one.
 
-function goToNextVideo(){
-	var videoEl = document.querySelector('#tutorialVideo');
-	video.src = "#";
 }
 
-
-function logIn(){
+function logIn() {
   var username = document.querySelector('#userName').value;
-  document.querySelector('#djNameOne').innerText = username;
-  location.href="index.html";
+  //document.querySelector('#djNameOne').innerText = username;
+  location.href = "index.html";
 }
 
-function signUp(){
+function signUp() {
   var username = document.querySelector('#userName').value;
-  document.querySelector('#djNameOne').innerText = username;
+  //document.querySelector('#djNameOne').innerText = username;
   alert("Thank you for signing up! " + username + "!");
-  location.href="index.html";
+  location.href = "index.html";
 }
+
+
+var num1 = 16,
+  num2 = 17;
 
 function saveMusic() {
-  var num = document.querySelector('#saveMusic').value;
-  num = num  + 1
-  document.querySelector('#saveMusic').innerHTML = `${num}`;
+  num1 = num1 + 1
+  document.querySelector('#num1').innerHTML = `${num1}`;
 }
 
 function likeMusic() {
-  var num = document.querySelector('#likeMusic').value;
-  num = num  + 1
-  document.querySelector('#saveMusic').innerHTML = `${num}`;
+  num2 = num2 + 1
+  document.querySelector('#num2').innerHTML = `${num2}`;
 }
